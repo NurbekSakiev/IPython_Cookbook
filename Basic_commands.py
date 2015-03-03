3efb488cff0fb502c4b9111051d1d4a929fcd915
NurbekSakiev <sakin.snn@gmail.com>
2015-03-02 18:13:55 -0600

    The basic commands of IPython

---
+++ Basic_commands.py
@@ -0,0 +1,7 @@
+print "Hello World" ## Press Shift+Enter to run the code
+2+2                 ## gives the result 4
+_ * 2               ## returns 4.  
+					## _(underscore) recovers the value of the last returned object
+!ls *.ipynb 		## !(exclamation mark) runs the shell commands
+%lsmagic			## lists the magic commands
+%%writefile			## creates a text file
